f0c19acbff75de8aee45ac5ed3152d5ca7698857
anderson <anderson_under@hotmail.com>
2026-05-24 19:52:45 -0400

    Pilha: empilhar digitos

--- Pilha e Fila/pilha_calculadora.c
+++ Pilha e Fila/pilha_calculadora.c
@@ -17,25 +17,44 @@ typedef struct pilha {
 void push(Pilha *, int);
 int pop(Pilha *);
 void imprimir(Pilha *);
+void empilhar_digitos(Pilha *, int);
 
 /******************** PRINCIPAL ********************/
 
 int main() {
     Pilha pilhaA = {}, pilhaB = {}, pilhaResultado = {};
 
-    push(&pilhaA, 10);
-    push(&pilhaB, 20);
-    push(&pilhaA, 30);
-    pop(&pilhaA);
-    imprimir(&pilhaB);
+    int digito1, digito2;
+    puts("Digite dois numeros inteiros.");
+    scanf("%d", &digito1);
+    scanf("%d", &digito2);
 
+    empilhar_digitos(&pilhaA, digito1);
+    empilhar_digitos(&pilhaB, digito2);
+
+    printf("%d", digito1);
+    printf(" + ");
+    printf("%d", digito2);
+    printf(" = ");
+
+    int vai_um = 0;
+    while (pilhaA.topo != NULL || pilhaB.topo != NULL || vai_um > 0) {
+        int digito1 = pop(&pilhaA);
+        int digito2 = pop(&pilhaB);
+        int soma = digito1 + digito2 + vai_um;
+
+        push(&pilhaResultado, soma % 10);
+        vai_um = soma / 10;
+    }
+
+    imprimir(&pilhaResultado);
     return 0;
 }
 
 /******************** FUNÇÕES PILHA ********************/
 
 void push(Pilha *s, int value) {
-    No *novo = malloc(sizeof(No));
+    No *novo = (No *)malloc(sizeof(No));
     if (!novo) { printf("Falha ao Alocar"); return; }
 
     novo->value = value;
@@ -44,7 +63,7 @@ void push(Pilha *s, int value) {
 }
 
 int pop(Pilha *s) {
-    if (!s->topo) { printf("Pilha Vazia\n"); return 0; }
+    if (!s->topo) { return 0; }
 
     No *aux = s->topo;
     int value = s->topo->value;
@@ -59,7 +78,20 @@ void imprimir(Pilha *s) {
     if (!aux) { printf("Pilha Vazia"); return; }
 
     while (aux) {
-        printf("\n%d\n", aux->value);
+        printf("%d", aux->value);
         aux = aux->next;
+    }
+}
+
+void empilhar_digitos(Pilha *s, int valor) {
+    if (valor == 0) {
+        push(s, 0);
+        return;
+    }
+    char num_str[16];
+    itoa(valor, num_str, 10);
+
+    for (int i = 0; num_str[i] != '\0'; i++) {
+        push(s, num_str[i] - '0');
     }
 }
